58c46ba45940f093359ea44b209ef51776790b62
Pablo Santos <pabsanort2@alum.us.es>
2021-05-02 18:02:30 +0200

    Arreglo de validaciones

--- src/main/java/acme/features/manager/task/ManagerTaskUpdateService.java
+++ src/main/java/acme/features/manager/task/ManagerTaskUpdateService.java
@@ -1,9 +1,11 @@
 
 package acme.features.manager.task;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.stereotype.Service;
 
 import acme.entities.Task;
@@ -64,36 +66,68 @@ public class ManagerTaskUpdateService implements AbstractUpdateService<Manager, 
 		assert entity != null;
 		assert errors != null;
 
-		final List<Word> palabrasSpam = this.repository.spWords();
-
-		final String pal = entity.getTitle().trim();
-		final Integer tamA = entity.getTitle().split(" ").length;
-
-		final String pal2 = entity.getText().trim();
-		final Integer tamT = entity.getText().split(" ").length;
-
-		Integer acumA = 0;
-		Integer acumT = 0;
-
-		for (int i = 0; i < palabrasSpam.size(); i++) {
-
-			if (pal.contains(palabrasSpam.get(i).getWord())) {
-				acumA++;
-			}
-			if (pal2.contains(palabrasSpam.get(i).getWord())) {
-				acumT++;
-			}
-
+		try {
+			request.getModel().getDate("start");
+		} catch (final ConversionFailedException e) {
+			errors.state(request, false, "start", "manager.task.error.task-parse");
 		}
-		final double spamA = acumA * 100 / tamA;
-		final double spamT = acumT * 100 / tamT;
 
-		final Double umbral = this.repository.findSpam().getThreshold();
-		final boolean isShoutSpamA = spamA > umbral;
-		final boolean isShoutSpamT = spamT > umbral;
-		errors.state(request, !isShoutSpamA, "title", "manager.task.error.task-spam");
-		errors.state(request, !isShoutSpamT, "text", "manager.task.error.task-spam");
+		try {
+			request.getModel().getDate("end");
+		} catch (final ConversionFailedException e) {
+			errors.state(request, false, "end", "manager.task.error.task-parse");
+		}
 
+		if (!errors.hasErrors("start") && !errors.hasErrors("end")) {
+			final Date end = request.getModel().getDate("end");
+			final Date start = request.getModel().getDate("start");
+
+			if (end == null || start == null) {
+				errors.state(request, !(start == null), "start", "manager.task.error.task-date");
+				errors.state(request, !(end == null), "end", "manager.task.error.task-date");
+			} else {
+				errors.state(request, end.after(start), "end", "manager.task.error.task-dateAfter");
+			}
+		}
+		
+		final int textLength = request.getModel().getString("text").length();
+		errors.state(request, textLength > 0 && textLength <= 100, "text", "acme.validation.length", 1, 100);
+
+		final int authorLength = request.getModel().getString("title").length();
+		errors.state(request, authorLength >= 5 && authorLength <= 25, "title", "acme.validation.length", 5, 25);
+
+		if (!errors.hasErrors("text") && !errors.hasErrors("title")) {
+
+			final List<Word> palabrasSpam = this.repository.spWords();
+
+			final String pal = entity.getTitle().trim();
+			final Integer tamA = entity.getTitle().split(" ").length;
+
+			final String pal2 = entity.getText().trim();
+			final Integer tamT = entity.getText().split(" ").length;
+
+			Integer acumA = 0;
+			Integer acumT = 0;
+
+			for (int i = 0; i < palabrasSpam.size(); i++) {
+
+				if (pal.contains(palabrasSpam.get(i).getWord())) {
+					acumA++;
+				}
+				if (pal2.contains(palabrasSpam.get(i).getWord())) {
+					acumT++;
+				}
+
+			}
+			final double spamA = acumA * 100 / tamA;
+			final double spamT = acumT * 100 / tamT;
+
+			final Double umbral = this.repository.findSpam().getThreshold();
+			final boolean isShoutSpamA = spamA > umbral;
+			final boolean isShoutSpamT = spamT > umbral;
+			errors.state(request, !isShoutSpamA, "title", "manager.task.error.task-spam");
+			errors.state(request, !isShoutSpamT, "text", "manager.task.error.task-spam");
+		}
 	}
 
 	@Override
@@ -101,15 +135,15 @@ public class ManagerTaskUpdateService implements AbstractUpdateService<Manager, 
 		assert request != null;
 		assert entity != null;
 
-		final Double actualWorkload = entity.getWorkLoad(); 
+		final Double actualWorkload = entity.getWorkLoad();
 		final Integer minutes = this.getMinutes(actualWorkload);
-		
+
 		entity.setWorkLoad(this.toHours(minutes));
-		
+
 		this.repository.save(entity);
-		
+
 	}
-	
+
 	public Integer getMinutes(final Double workload) {
 		final Double parteEntera = Math.floor(workload);
 		final Double parteDecimal = workload - parteEntera;
